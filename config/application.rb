3847ae996b9793138897d510898850afe25b6f85
louis7 <louisvis@ymail.com>
2020-09-14 23:50:53 -0400

    added config/initialize/cors.rb to solve the cors issue

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(*Rails.groups)
 
 module BeatsByRails
   class Application < Rails::Application
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
